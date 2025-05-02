1edf3df840159e26632b1792cccc2fc54706a1cd
Mahdi Jabbary <Mahdijabbary505@gmail.com>
2025-05-03 03:23:21 +0330

    Forkify v1 is done!

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.jonas.io/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5ece1da4-fdcc-4f7b-815b-472d60f2ab0b';
+export const MODAL_CLOSE_SEC = 2.5;
